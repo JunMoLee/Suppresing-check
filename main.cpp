3bef4a6dffa3bba482fd48aca256b00d1d13c515
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-02-27 03:42:01 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -160,14 +160,14 @@ int main() {
 		cout << "Training Epoch : " << i << endl; 
 		if(i<=20)
 		{
-	        param->ChangeLearningrate(0.2, 0.2, 0.2);
-		Train(param->numTrainImagesPerEpoch, param->interNumEpochs,param->optimization_type,0,0,1,1,0.2);
-		cout<<"alpha1 "<< param->alpha1 <<" dalpha "<<param->dalpha<<" nalpha1 "<<param->nalpha1<<" nur "<<(int)(param->newUpdateRate/0.2)<<endl;}
+	        param->ChangeLearningrate(0.35-0.15*(i-1)/20, 0.35-0.15*(i-1)/20, 0.35-0.15*(i-1)/20, 0.35-0.15*(i-1)/20);
+		Train(param->numTrainImagesPerEpoch, param->interNumEpochs,param->optimization_type,0);
+		cout<<"alpha1 "<< param->alpha1 <<" dalpha "<<param->dalpha<<" nalpha1 "<<param->nalpha1<<" palpha "<<param->palpha<<" nur "<<(int)(param->newUpdateRate/0.2)<<endl;}
 		else
 		{
-		param->ChangeLearningrate(0.2, 0.2, 0.2);
-		Train(param->numTrainImagesPerEpoch, param->interNumEpochs,param->optimization_type,1);
-		cout<<"alpha1 "<< param->alpha1 <<" dalpha "<<param->dalpha<<" nalpha1 "<<param->nalpha1<<" nur "<<param->newUpdateRate<<endl;}
+		param->ChangeLearningraNte(0.2, 0.2, 0.2, 0.2);
+		Train(param->numTrainImagesPerEpoch, param->interumEpochs,param->optimization_type,0);
+		cout<<"alpha1 "<< param->alpha1 <<" dalpha "<<param->dalpha<<" nalpha1 "<<param->nalpha1<<" palpha "<<param->palpha<<" nur "<<(int)(param->newUpdateRate/0.2)<<endl;}
 		if (!param->useHardwareInTraining && param->useHardwareInTestingFF) { WeightToConductance(); }
 		Validate();
 		if(write_or_not){
