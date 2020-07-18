104e4e688bfdc2f51891fc6e7ef49b6a50c5ba00
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-07-18 22:32:32 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -178,16 +178,7 @@ int main() {
 		Train(param->numTrainImagesPerEpoch, param->interNumEpochs,param->optimization_type,i);
 		cout<<"alpha1 "<< param->alpha1 <<" dalpha "<<param->dalpha<<" nalpha1 "<<param->nalpha1<<" pdalpha "<<param->pdalpha<<" nur "<<(int)(param->newUpdateRate)<<endl;
 		
-		if (i>=101)
-		{       accuracy[(size_t)i-101] = (double)correct/param->numMnistTestImages*100
-			averagesum += accuracy[(size_t)i-101];
-			cout<<"accumulated average accuracy : "<<averagesum/(i-100)<<endl;
-		        for(size_t j=101; j<=i;j++){
-			stdsum += ( accuracy[(size_t)j-101] - averagesum/(i-100) ) * ( accuracy[(size_t)j-101] - averagesum/(i-100) );
-			}
-			cout<<"accumulated standard deviation : "<<sqrt(stdsum/(i-100))<<endl;
-			
-		}
+		
 		//end of simulation case 0
 		}
 		break;
@@ -455,6 +446,16 @@ cout<<"alpha1 "<< param->alpha1 <<" dalpha "<<param->dalpha<<" nalpha1 "<<param-
 		
 		}
 		printf("%.2f\n", (double)correct/param->numMnistTestImages*100);
+		if (i>=101)
+		{       accuracy[(size_t)i-101] = (double)correct/param->numMnistTestImages*100;
+			averagesum += accuracy[(size_t)i-101];
+			cout<<"accumulated average accuracy : "<<averagesum/(i-100)<<endl;
+		        for(size_t j=101; j<=i;j++){
+			stdsum += ( accuracy[(size_t)j-101] - averagesum/(i-100) ) * ( accuracy[(size_t)j-101] - averagesum/(i-100) );
+			}
+			cout<<"accumulated standard deviation : "<<sqrt(stdsum/(i-100))<<endl;
+			
+		}
 		/*printf("\tRead latency=%.4e s\n", subArrayIH->readLatency + subArrayHO->readLatency);
 		printf("\tWrite latency=%.4e s\n", subArrayIH->writeLatency + subArrayHO->writeLatency);
 		printf("\tRead energy=%.4e J\n", arrayIH->readEnergy + subArrayIH->readDynamicEnergy + arrayHO->readEnergy + subArrayHO->readDynamicEnergy);
