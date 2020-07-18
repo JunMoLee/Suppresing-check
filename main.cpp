55ceef582bb41282d37b0c530446deecac74e41a
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-07-19 00:28:19 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -137,7 +137,8 @@ int main() {
 		bool write_or_not=1;
 		fstream read;
 		read.open("SI_200718_PCMrefresh-onlycheck.csv",fstream::app);                                                         
-																
+		vector <double> accuracy (25,0);
+	
 		for (int i=1; i<=125; i++) {
 		double NL_LTP_Gp = static_cast<RealDevice*>(arrayIH->cell[0][0])->NL_LTP_Gp;
 	        double NL_LTD_Gp = static_cast<RealDevice*>(arrayIH->cell[0][0])->NL_LTD_Gp;
@@ -153,7 +154,7 @@ int main() {
 	        double LAd = param->dalpha;
 	        double pLAd = param->pdalpha;
 	        double nLA = param->nalpha1;
-		vector <double> accuracy (25,0);
+		
 		double averagesum=0;
 		double stdsum=0;
 	        double wv = (static_cast<RealDevice*>(arrayIH->cell[0][0])->maxConductance - static_cast<RealDevice*>(arrayIH->cell[0][0])->minConductance)*0.015;
