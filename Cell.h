25b6347949f5917e0cb224e2afb347cf977b1322
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-07-24 17:00:30 +0900

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -151,7 +151,7 @@ public:
 	double writeVoltageSquareSum;   // Sum of V^2 of non-identical pulses (for weight update energy calculation in subcircuits)
 
 	virtual double Read(double voltage) = 0;
-	virtual void Write(double deltaWeightNormalized, double weight, double minWeight, double maxWeight) = 0;
+	virtual void Write(double deltaWeightNormalized, double weight, double minWeight, double maxWeight, double* learningrate = 0) = 0;
 	double GetMaxReadCurrent(){
 		if (cmosAccess)
 			return readVoltage * 1 / (1 / avgMaxConductance);//+resistanceAccess);
@@ -233,7 +233,7 @@ public:
 	double NL_LTN_Gn;
 	MeasuredDevice(int x, int y);
 	double Read(double voltage);	// Return read current (A)
-	void Write(double deltaWeightNormalized, double weight, double minWeight, double maxWeight);
+	void Write(double deltaWeightNormalized, double weight, double minWeight, double maxWeight, double* learningrate = 0);
 };
 
 #endif
