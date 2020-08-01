2b80857691429f563240ca6e864ca71837801d0e
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-08-02 00:06:46 +0900

    Update Train.cpp

--- Train.cpp
+++ Train.cpp
@@ -598,12 +598,12 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 							   {
 							      if(updatepattern1[areanumber1][0]*1000+updatepattern1[areanumber1][1]*100+updatepattern1[areanumber1][2]*10+updatepattern1[areanumber1][3] == 2211)
 							      {
-							      learningrateIH[0] = param->learningrate[0][0]*1.5;
+							      learningrateIH[0] = param->learningrate[0][0];
 							      learningrateIH[1] = param->learningrate[0][1];
 							      learningrateIH[2] = param->learningrate[0][2];
 							      learningrateIH[3] = param->learningrate[0][3];
-							      posstopreverse=1;
-						              negstopreverse=0;
+							     posstopreverse=1;
+						              negstopreverse=1;
 							      }
 			                                      else if(updatepattern1[areanumber1][0]*1000+updatepattern1[areanumber1][1]*100+updatepattern1[areanumber1][2]*10+updatepattern1[areanumber1][3] == 2233)
 							      {
@@ -611,8 +611,8 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 							      learningrateIH[1] = param->learningrate[0][1];
 							      learningrateIH[2] = param->learningrate[0][2];
 							      learningrateIH[3] = param->learningrate[0][3];
-							      posstopreverse=1;
-						              negstopreverse=0;
+							     posstopreverse=1;
+						              negstopreverse=1;
 							      }
 				                              else
 							      {
@@ -628,7 +628,7 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 					                   else {
 				                              if(updatepattern1[areanumber1][0]*1000+updatepattern1[areanumber1][1]*100+updatepattern1[areanumber1][2]*10+updatepattern1[areanumber1][3] == 1111)
 							      {
-				                              learningrateIH[0] = param->learningrate[0][0]*1.5;
+				                              learningrateIH[0] = param->learningrate[0][0];
 							      learningrateIH[1] = param->learningrate[0][1];
 							      learningrateIH[2] = param->learningrate[0][2];
 							      learningrateIH[3] = param->learningrate[0][3];
@@ -1126,12 +1126,12 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 				                            if((batchSize+numTrain*(epochcount-1))==param->TrackRate*2-1){
 							      if(updatepattern2[areanumber2][0]*1000+updatepattern2[areanumber2][1]*100+updatepattern2[areanumber2][2]*10+updatepattern2[areanumber2][3] == 2211)
 							      {
-							      learningrateHO[0] = param->learningrate[1][0]*1.5;
+							      learningrateHO[0] = param->learningrate[1][0];
 							      learningrateHO[1] = param->learningrate[1][1];
 							      learningrateHO[2] = param->learningrate[1][2];
 							      learningrateHO[3] = param->learningrate[1][3];
-							      posstopreverse=1;
-						              negstopreverse=0;
+						     posstopreverse=1;
+						              negstopreverse=1;
 							      }
 				                              else if(updatepattern2[areanumber2][0]*1000+updatepattern2[areanumber2][1]*100+updatepattern2[areanumber2][2]*10+updatepattern2[areanumber2][3] == 2233)
 							      {
@@ -1139,7 +1139,7 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 							      learningrateHO[1] = param->learningrate[1][1];
 							      learningrateHO[2] = param->learningrate[1][2];
 							      learningrateHO[3] = param->learningrate[1][3];
-							      posstopreverse=0;
+						     posstopreverse=1;
 						              negstopreverse=1;
 							      }
 				                              else
@@ -1156,7 +1156,7 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 				                            else {
 				                              if(updatepattern2[areanumber2][0]*1000+updatepattern2[areanumber2][1]*100+updatepattern2[areanumber2][2]*10+updatepattern2[areanumber2][3] == 1111)
 							      {
-							      learningrateHO[0] = param->learningrate[1][0]*1.5;
+							      learningrateHO[0] = param->learningrate[1][0];
 							      learningrateHO[1] = param->learningrate[1][1];
 							      learningrateHO[2] = param->learningrate[1][2];
 							      learningrateHO[3] = param->learningrate[1][3];
