4d1e5555701a5a37d03e8bf546b80db75f38eae2
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-08-03 01:18:19 +0900

    Update Train.cpp

--- Train.cpp
+++ Train.cpp
@@ -1597,7 +1597,7 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 		        }
 				for (int e=0; e<100;e++){
 					
-							cout<<"   "<<"a["<<e<<"]="<<scaling(a1[e])<<"   "<<"s["<<e<<"]="<<scaling(s1[1])<<endl;
+							cout<<"   "<<"a["<<e<<"]="<<scaling(a1[e])<<"   "<<"s["<<e<<"]="<<scaling(s1[e])<<endl;
 				}
 				
 				cout<<endl;
