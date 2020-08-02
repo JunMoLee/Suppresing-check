116286eb88d7eafe5ffaafe8cda63f5bee91e8fa
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-08-02 20:46:03 +0900

    Update Train.cpp

--- Train.cpp
+++ Train.cpp
@@ -620,7 +620,7 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 				                              if((updatepattern[areanum][0]*1000+updatepattern[areanum][1]*100+updatepattern[areanum][2]*10+updatepattern[areanum][3] == 1111) && (reset==1))
 							      {
 				                              learningrateIH[0] = param->learningrate[0][0];
-							      learningrateIH[1] = param->learningrate[0][1];
+							      learningrateIH[1] = param->learningrate[0][1]/2;
 							      learningrateIH[2] = param->learningrate[0][2];
 							      learningrateIH[3] = param->learningrate[0][3];
 							      posstopreverse=1;
@@ -1166,7 +1166,7 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 				                              if((updatepattern[areanum][0]*1000+updatepattern[areanum][1]*100+updatepattern[areanum][2]*10+updatepattern[areanum][3] == 1111)  && (reset==1))
 							      {
 				                              learningrateHO[0] = param->learningrate[0][0];
-							      learningrateHO[1] = param->learningrate[0][1];
+							      learningrateHO[1] = param->learningrate[0][1]/2;
 							      learningrateHO[2] = param->learningrate[0][2];
 							      learningrateHO[3] = param->learningrate[0][3];
 							      posstopreverse=1;
