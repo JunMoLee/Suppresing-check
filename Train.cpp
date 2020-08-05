69cc689ede9bdde7c9e32f190e8b31be35e9eb72
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-08-05 17:08:24 +0900

    Update Train.cpp

--- Train.cpp
+++ Train.cpp
@@ -1772,12 +1772,12 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 				    prevweightsum[areanum22] = weightsum[areanum22];
                           }
 		
-			/* for (int e=0; e<10;e++){
+			 for (int e=0; e<10;e++){
 					
 							cout<<"   "<<"a["<<e<<"]="<<scaling(a2[e])<<"   "<<"s["<<e<<"]="<<scaling(s2[e])<<endl;
-				} */
+				} 
 				
-				 /* cout<<endl; */
+				cout<<endl; 
 				
 			/* cout << "epoch : "<<epochcount << " batchSize : " <<batchSize<<endl;
 			cout <<"avg IH positive sat: " << possatsum1/param->RefreshRate/40000<< ", " <<possatsum1/param->RefreshRate/40000*100<<"%";
