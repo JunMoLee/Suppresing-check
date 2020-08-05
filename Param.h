c168d62324ac6bb0b36d95eac7b07de780131ffe
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-08-05 15:21:14 +0900

    Update Param.h

--- Param.h
+++ Param.h
@@ -61,7 +61,8 @@ public:
 	int nInput;     // # of neurons in input layer
 	int nHide;      // # of neurons in hidden layer
 	int nOutput;	// # of neurons in output layer
-		 int allocationmethod=0;
+	 int allocationmethodIH=0;
+	 int allocationmethodHO=0;
 	
 	double alpha1;		// Learning rate for the synapses from input to hidden layer
 	double alpha2;		// Learning rate for the synapses from hidden to output layer
