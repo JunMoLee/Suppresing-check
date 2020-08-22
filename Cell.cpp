db26b60240f9915f94bc0d07c8e9217516f25783
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-08-22 18:20:21 +0900

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -715,7 +715,7 @@ void RealDevice::newWrite(double deltaWeightNormalized, double weight, double mi
 	double pcondrange = pmaxConductance - pminConductance;
 	double ncondrange = nmaxConductance - nminConductance;
 	double deltaWeightSign;
-	double realupdate
+	double realupdate;
 	
 	bool GpGnCell = true;
 	if (positiveupdate && (deltaWeightNormalized > 0)) {	// + reverse update
