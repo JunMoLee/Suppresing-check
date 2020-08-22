7689bde617a58feda9ca67cca8b5a00b6f8f7dcf
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-08-23 03:53:18 +0900

    Update Param.cpp

--- Param.cpp
+++ Param.cpp
@@ -493,14 +493,30 @@ param_gn=-5;
 							newUpdateRate=12;
 
 	nnewUpdateRate=4;
-		RefreshRate = 199;
+		RefreshRate = 100;
 		        ReverseUpdate = 0;
 
 	FullRefresh= 1;
 break;
 case 8:
-param_gp=8;
-param_gn=-1;
+param_gp=3;
+param_gn=-5;
+						learningrate[0][0] =0.4;
+	learningrate[0][1] = 0.4;
+	learningrate[0][2] = 0.33333;
+	learningrate[0][3] = 0.4;
+		
+	learningrate[1][0] = 0.4;
+	learningrate[1][1] = 0.4;
+	learningrate[1][2] = 0.33333;
+	learningrate[1][3] = 0.4;
+							newUpdateRate=12;
+
+	nnewUpdateRate=4;
+		RefreshRate = 100;
+		        ReverseUpdate = 0;
+
+	FullRefresh= 1;
 break;
 case 9:
 param_gp=5;
